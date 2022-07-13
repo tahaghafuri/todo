fb5eb20947fddfcdbf0315dd5a87c068120cd161
tahaghafuri <tahaghafuri@gmail.com>
2022-07-13 19:33:21 +0430

    Fix Routes In web.php

--- routes/web.php
+++ routes/web.php
@@ -23,28 +23,28 @@ Route::get('/', function(){
 
 Route::get('/cat', [CatController::class, 'index']);
 
-Route::get('/cat/create', [CatController::class, 'create']);
-
-Route::post('/cat/store-data', [CatController::class, 'store'])->name('cat.store');
-
-Route::get('/cat/details/{cat}', [CatController::class, 'details']);
-
-Route::get('/cat/edit/{cat}', [CatController::class, 'edit']);
-
-Route::post('/cat/update/{cat}', [CatController::class, 'update']);
-
-Route::get('/cat/delete/{cat}', [CatController::class, 'delete']);
-
 Route::get('/todo', [TodoController::class, 'index']);
+
+Route::get('/cat/create', [CatController::class, 'create']);
 
 Route::get('/todo/create', [TodoController::class, 'create']);
 
+Route::post('/cat/store-data', [CatController::class, 'store'])->name('cat.store');
+
 Route::post('/todo/store-data', [TodoController::class, 'store'])->name('todo.store');
+
+Route::get('/cat/details/{cat}', [CatController::class, 'details']);
 
 Route::get('/todo/details/{todo}', [TodoController::class, 'details']);
 
+Route::get('/cat/edit/{cat}', [CatController::class, 'edit']);
+
 Route::get('/todo/edit/{todo}', [TodoController::class, 'edit']);
 
+Route::post('/cat/update/{cat}', [CatController::class, 'update']);
+
 Route::post('/todo/update/{todo}', [TodoController::class, 'update']);
+
+Route::get('/cat/delete/{cat}', [CatController::class, 'delete']);
 
 Route::get('/todo/delete/{todo}', [TodoController::class, 'delete']);
