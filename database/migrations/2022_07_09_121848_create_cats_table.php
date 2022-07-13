0c7ca0eb0ed66004a7917b842cd69c55c1395a6b
tahaghafuri <tahaghafuri@gmail.com>
2022-07-13 16:41:04 +0430

    Add Cats Table Columns
    Added Cats Table Columns:
    id,title,desc

--- database/migrations/2022_07_09_121848_create_cats_table.php
+++ database/migrations/2022_07_09_121848_create_cats_table.php
@@ -15,6 +15,8 @@ return new class extends Migration
     {
         Schema::create('cats', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->string('description')->nullable();
             $table->timestamps();
         });
     }
